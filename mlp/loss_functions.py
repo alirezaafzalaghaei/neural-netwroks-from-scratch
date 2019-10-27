cdac618c01cce7a5e883ff398f5202df1edd2c8c
Alireza <alirezaafzalaghaei@gmail.com>
2019-10-27 16:53:54 +0330

    added MSE adn XEntropy loss functions

---
+++ mlp/loss_functions.py
@@ -0,0 +1,40 @@
+import abc
+import numpy as np
+
+
+class Loss(abc.ABC):
+    @staticmethod
+    @abc.abstractmethod
+    def loss(y,t):
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def loss_prime(y,t):
+        pass
+
+
+class MSE(Loss):
+    @staticmethod
+    def loss(y, t):
+        return .5 * np.sum((t - y) ** 2)
+
+    @staticmethod
+    def loss_prime(y,t):
+        return t-y
+
+
+class XEntropy(Loss):
+    @staticmethod
+    def loss(t,y):
+        indices = np.argmax(t, axis=1).astype(int)
+        probability = y[np.arange(len(y)), indices] #inputs[0, indices]
+        log = np.log(probability)
+        loss = -1.0 * np.sum(log) / len(log)
+        return loss
+
+    @staticmethod
+    def loss_prime(y, t):
+        return (t-y)/y.shape[0]
+
+
