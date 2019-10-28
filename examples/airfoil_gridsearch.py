Implemented automatic data scaling for MLP, bug fixes

examples/airfoil_gridsearch.py
@@ -7,8 +7,6 @@ def load_airfoil():
     data_set = np.loadtxt('../datasets/airfoil.dat')
     X = data_set[:, :-1]
     y = data_set[:, -1].reshape(-1, 1)
-    X = StandardScaler().fit_transform(X)
-    y = StandardScaler().fit_transform(y)
 
     return X, y
 
@@ -26,7 +24,7 @@ X, y = load_airfoil()
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 t = time.time()
 mlp = MLPGridSearch('regression', hidden_layers, activations, batch_sizes, epochs, mus, betas, etas, alphas)
-histories = mlp.run(X_train, y_train)
+histories = mlp.run(X_train, y_train, X_test, y_test)
 t = time.time() - t
 print('time taken = %.2f sec' % t)
 

examples/airfoil_test.py
@@ -5,8 +5,8 @@ def load_airfoil():
     data_set = np.loadtxt('./datasets/airfoil.dat')
     X = data_set[:, :-1]
     y = data_set[:, -1].reshape(-1, 1)
-    X = StandardScaler().fit_transform(X)
-    y = StandardScaler().fit_transform(y)
+    # X = StandardScaler().fit_transform(X)
+    # y = StandardScaler().fit_transform(y)
 
     return X, y
 

mlp/grid_search.py
@@ -5,11 +5,12 @@ from utils import pd, np
 from pathos.multiprocessing import ProcessPool as Pool
 
 
-def MLP_model(hidden_layer, activation, epoch, eta, beta, alpha, mu, batch_size, task, x, y):
+def MLP_model(hidden_layer, activation, epoch, eta, beta, alpha, mu, batch_size, task, x, y, xt, yt):
     print('.', end='', flush=True)
     mlp = MLP(hidden_layer, activation, epoch, eta, beta, alpha, mu, batch_size, False, task)
     hist = mlp.fit(x, y)
-    return hist
+    score = mlp.score(xt, yt)
+    return hist, score
 
 
 class MLPGridSearch:
@@ -25,32 +26,34 @@ class MLPGridSearch:
         self.alphas = alphas
         self.csv = 'result.csv'
 
-    def run(self, x, y):
+    def run(self, x, y, xt, yt):
         file = open(self.csv, 'w')
         writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(
-            ['hidden_layer', 'activation', 'epoch', 'eta', 'beta', 'alpha', 'mu', 'batch_size', 'history'])
+            ['hidden_layer', 'activation', 'epoch', 'eta', 'beta', 'alpha', 'mu', 'batch_size','test_score', 'history'])
         combinations = tuple(
             itertools.product(self.hidden_layers, self.activations, self.epochs, self.etas, self.betas, self.alphas,
                               self.mus, self.batch_sizes))
         print("number of different models = %d" % len(combinations))
         print('.' * len(combinations))
-        histories = Pool().map(lambda p: MLP_model(*p, self.task, x, y), combinations)
-        for hist, cfg in zip(histories, combinations):
-            writer.writerow([*cfg, hist])
+        histories = Pool().map(lambda p: MLP_model(*p, self.task, x, y, xt, yt), combinations)
+        for (hist, loss), cfg in zip(histories, combinations):
+            writer.writerow([*cfg, loss, hist])
         file.close()
         print()
         return histories
 
     def best_model(self):
-        df = pd.read_csv('result.csv')
+        df = pd.read_csv(self.csv)
         df['history'] = df['history'].apply(lambda x:
                                             np.fromstring(
                                                 x.replace('\n', '').replace('[', '').replace(']', '').replace('  ',
                                                                                                               ' '),
                                                 sep=','))
-        losses = np.array([np.abs(loss[-5:]).mean() for loss in df['history']])
-        index = losses.argmin()
+        # losses = np.array([np.abs(loss[-5:]).mean() for loss in df['history']])
+        # index = losses.argmin()
+
+        index = df['test_score'].to_numpy().argmax()
         return dict(df.iloc[index, :])
 
 # todo: rank models

mlp/mlp.py
@@ -7,6 +7,7 @@ import numpy as np
 from .activations import *
 from .loss_functions import *
 from sklearn.metrics import r2_score, accuracy_score
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 np.random.seed(1)
 
@@ -105,6 +106,8 @@ class MLP:
         return self._loss(y, t) + self.alpha * np.sum([w.sum() for w in self.weights_])
 
     def fit(self, x, y):
+        x, y = self._encoder(x, y, fit=True)
+
         self.hidden_layer_sizes.insert(0, x.shape[1])
         self.hidden_layer_sizes.append(y.shape[1])
 
@@ -136,12 +139,38 @@ class MLP:
             print("epoch %05d, loss: %06.2f" % (i + 1, self.current_loss))
 
     def predict(self, x):
+        x = self._encoder(x)
         x = np.hstack((x, np.ones((x.shape[0], 1))))
-        return self._forward(x)
+        y = self._forward(x)
+        y = self._decoder(y)
+        return y
 
     def score(self, x, y):
         yp = self.predict(x)
+
         if self.task == 'regression':
-            return r2_score(y,yp)
+            return r2_score(y, yp)
         else:
             return accuracy_score(y, yp)
+
+    def _encoder(self, x=None, y=None, fit=False):
+        if fit:
+            self.x_encoder = StandardScaler().fit(x)
+            if self.task == 'regression':
+                self.y_encoder = StandardScaler().fit(y)
+            else:
+                self.y_encoder = OneHotEncoder().fit(y)
+
+        if x is None:
+            y = self.y_encoder.transform(y)
+            return y.toarray() if self.task == 'classification' else y
+        if y is None:
+            return self.x_encoder.transform(x)
+        y = self.y_encoder.transform(y)
+        return self.x_encoder.transform(x), y.toarray() if self.task == 'classification' else y
+
+    def _decoder(self, y):
+        if self.task == 'classification':
+            y = y.argmax(axis=1)
+            y = np.eye(self.y.shape[1])[y]
+        return self.y_encoder.inverse_transform(y)