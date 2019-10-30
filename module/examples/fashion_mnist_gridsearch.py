23f096b5248f70c2626084e41727b1ddeb554063
Alireza <alirezaafzalaghaei@gmail.com>
2019-10-30 07:45:49 +0330

    added fashion mnist dataset

---
+++ module/examples/fashion_mnist_gridsearch.py
@@ -0,0 +1,55 @@
+import time
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import texttable
+import os
+os.environ["KERAS_BACKEND"] = "theano"
+from keras.datasets import fashion_mnist
+
+from nn.mlp import MLPGridSearch
+from nn.mlp.activations import LeakyReLu, Tanh
+
+sns.set()
+
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train.reshape(60000,784)
+    x_test = x_test.reshape(10000, 784)
+    y_train = y_train.reshape(-1,1)
+    y_test = y_test.reshape(-1, 1)
+    return x_train, x_test, y_train, y_test
+
+
+hidden_layers = [(5, 5, 5, 5)]
+activations = [Tanh()]
+batch_sizes = [32]
+epochs = [3]
+mus = [0.95]
+betas = [.1]
+etas = [.01, .4]
+alphas = [.001]
+
+X_train, X_test, y_train, y_test = load_mnist()
+t = time.time()
+mlp = MLPGridSearch('classification', hidden_layers, activations, batch_sizes, epochs, mus, betas, etas, alphas)
+histories = mlp.run(X_train, y_train, X_test, y_test)
+t = time.time() - t
+print('time taken = %.2f sec' % t)
+
+result = mlp.best_model()
+hist = result.pop('history')
+print('Best model is: ')
+
+tbl = texttable.Texttable()
+tbl.set_cols_align(["c", "c"])
+tbl.set_cols_valign(["c", "c"])
+tbl.add_rows([['Hyperparameter', 'Best value'], *list(result.items())])
+print(tbl.draw())
+
+plt.plot(list(range(len(hist))), hist)
+plt.title("loss: %.2e" % hist[-1])
+plt.xlabel('iterations')
+plt.ylabel('Log(loss)')
+plt.show()
---
+++ module/examples/fashion_mnist_test.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+os.environ["KERAS_BACKEND"] = "theano"
+from keras.datasets import fashion_mnist
+
+from nn.mlp import MLP
+from nn.mlp.activations import LeakyReLu
+
+sns.set()
+
+
+def load_mnist():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train.reshape(60000,784)
+    x_test = x_test.reshape(10000, 784)
+    y_train = y_train.reshape(-1,1)
+    y_test = y_test.reshape(-1, 1)
+    return x_train, x_test, y_train, y_test
+
+
+X_train, X_test, y_train, y_test = load_mnist()
+
+mlp = MLP([30, 10], activation=LeakyReLu(.1), batch_size=256, epochs=10, mu=0.95, beta=.2, eta=.5, alpha=.01,
+          verbose=1, task='classification')
+
+hist = mlp.fit(X_train, y_train)
+
+acc_test = mlp.score(X_test, y_test)
+acc_train = mlp.score(X_train, y_train)
+
+print('train accuracy: %.2f%%' % (acc_train * 100))
+print('test accuracy: %.2f%%' % (acc_test * 100))
+
+plt.plot(list(range(len(hist))), hist)
+plt.title("loss: %.2e" % hist[-1])
+plt.xlabel('iterations')
+plt.ylabel('Log(loss)')
+plt.show()
