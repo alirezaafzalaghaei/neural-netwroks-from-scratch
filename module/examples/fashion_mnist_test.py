8c1c5cb8b5b53975aa0a807aba5f162f953d4543
Alireza <alirezaafzalaghaei@gmail.com>
2019-11-04 20:30:11 +0330

    changed batch size

--- module/examples/fashion_mnist_test.py
+++ module/examples/fashion_mnist_test.py
@@ -7,6 +7,7 @@ from keras.datasets import fashion_mnist
 
 from nn.mlp import MLP
 from nn.mlp.activations import LeakyReLu
+from sklearn.model_selection import train_test_split
 
 sns.set()
 
@@ -20,20 +21,25 @@ def load_mnist():
     return x_train, x_test, y_train, y_test
 
 
-X_train, X_test, y_train, y_test = load_mnist()
+X, X_test, y, y_test = load_mnist()
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.15)
 
-mlp = MLP([30, 10], activation=LeakyReLu(.1), batch_size=256, epochs=10, mu=0.95, beta=.2, eta=.5, alpha=.01,
+
+mlp = MLP([30, 10], activation=LeakyReLu(.1), batch_size=64, epochs=30, mu=0.95, beta=.2, eta=.3, alpha=.01,
           verbose=1, task='classification')
 
-hist = mlp.fit(X_train, y_train)
-
+hist, valid = mlp.fit(X_train, y_train, validation=(X_valid, y_valid))
+valid = valid[:, 1]
+hist = hist[:, 1]
 acc_test = mlp.score(X_test, y_test)
 acc_train = mlp.score(X_train, y_train)
 
 print('train accuracy: %.2f%%' % (acc_train * 100))
 print('test accuracy: %.2f%%' % (acc_test * 100))
 
-plt.plot(list(range(len(hist))), hist)
+plt.plot(list(range(len(hist))), hist, label='train')
+plt.plot(list(range(len(hist))), valid, label='valid')
+plt.legend()
 plt.title("loss: %.2e" % hist[-1])
 plt.xlabel('iterations')
 plt.ylabel('Log(loss)')
